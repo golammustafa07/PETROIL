3533165c4cb341d3f99a6adf88ffea719bccd973
golammustafa07 <tapatap1234@gmail.com>
2024-03-29 03:52:39 +0600

    Upto compnay part done with responsive

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -5,14 +5,14 @@ import company from '../../assets/company.png'
 const Company = () => {
     return (
         <div>
-            <Container className='py-[136px]'>
-                <div className='flex'>
-                    <div className='bg-primary py-[100px] px-[75.5px]'>
-                        <h2 className='font-pops font-bold text-[36px] text-white leading-[54px] w-[262px]'>Learn more about our company</h2>
+            <Container className='py-[50px] xl:py-[136px]'>
+                <div className='flex flex-col lg:flex-row gap-y-2.5'>
+                    <div className='bg-primary py-[50px] px-[50px] lg:py-[100px] lg:px-[75.5px] text-center lg:text-left'>
+                        <h2 className='font-pops font-bold text-xl md:text-[36px] text-white lg:leading-[54px] w-auto lg:w-[262px]'>Learn more about our company</h2>
                     </div>
 
                     <div className='relative'>
-                        <img className='h-[410px]' src={company} alt="" />
+                        <img className='w-full lg:h-[410px]' src={company} alt="" />
                         <div className='w-full h-full flex justify-center items-center top-0 absolute'>
                             <button className='font-pops font-semibold text-base py-3.5 px-[30px] bg-white text-primary hover:text-white hover:bg-primary'>Learn More</button>
                         </div>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -11,7 +11,7 @@ const Services = () => {
         <div className='flex flex-col gap-y-6 lg:gap-y-0'>
           <div className='flex flex-wrap justify-end items-center gap-y-8'>
             <div className='lg:w-1/2'>
-              <div className='text-center xl:ml-[380px] md:text-left'>
+              <div className='text-center xl:ml-[80px] 2xl:ml-[380px] lg:text-left'>
                 <h2 className='font-bold font-pops text-2xl lg:text-[64px] lg:leading-[96px] w-auto lg:w-[509px]'>Our Services</h2>
                 <p className='font-pops font-medium text-base w-auto lg:w-[405px]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
               </div>
@@ -22,7 +22,7 @@ const Services = () => {
                 <img src={servicesone} alt="" />
                 <div className='w-full flex justify-center items-center h-full absolute top-0 bg-overlay'>
                   <div className='text-center lg:text-left py-[50px] md:py-0 px-3 lg:px-0'>
-                    <h3 className='font-pops font-bold text-xl lg:text-2xl xl:text-[36px] text-white xl:leading-[54px]'>Modern natural oil and gas refineries.</h3>
+                    <h3 className='font-pops font-bold text-2xl md:text-3xl lg:text-2xl 2xl:text-[36px] text-white xl:leading-[54px]'>Modern natural oil and gas refineries.</h3>
                     <button className='py-2.5 px-2.5 md:py-3.5 md:px-[30px] bg-primary rounded-full md:rounded-none text-white hover:bg-green-500 mt-5'>Learn More</button>
                   </div>
                 </div>
@@ -35,7 +35,7 @@ const Services = () => {
                 <img src={servicestwo} alt="" />
                 <div className='w-full flex justify-center items-center h-full absolute top-0 bg-overlay'>
                   <div className='text-center lg:text-left py-[50px] md:py-0'>
-                    <h3 className='font-pops font-bold text-xl lg:text-2xl xl:text-[36px] text-white xl:leading-[54px]'>Supply of national industries.</h3>
+                    <h3 className='font-pops font-bold text-2xl md:text-3xl lg:text-2xl 2xl:text-[36px] text-white xl:leading-[54px]'>Supply of national industries.</h3>
                     <button className='py-2.5 px-2.5 md:py-3.5 md:px-[30px] bg-primary rounded-full md:rounded-none text-white hover:bg-green-500 mt-5'>Learn More</button>
                   </div>
                 </div>
@@ -44,10 +44,10 @@ const Services = () => {
 
             <div className='lg:w-1/2'>
               <div className='relative'>
-                <img className='lg:h-[225px] xl:h-[410px]' src={servicesthree} alt="" />
+                <img className='lg:h-[225px] xl:h-[286px] 2xl:h-[410px]' src={servicesthree} alt="" />
                 <div className='w-full flex justify-center items-center h-full absolute top-0 bg-overlay'>
                   <div className='text-center lg:text-left py-[50px] md:py-0'>
-                    <h3 className='font-pops font-bold text-xl lg:text-2xl xl:text-[36px] text-white xl:leading-[54px]'>National fuel distribution and supply.</h3>
+                    <h3 className='font-pops font-bold text-2xl md:text-3xl lg:text-2xl 2xl:text-[36px] text-white xl:leading-[54px]'>National fuel distribution and supply.</h3>
                     <button className='py-2.5 px-2.5 md:py-3.5 md:px-[30px] bg-primary rounded-full md:rounded-none text-white hover:bg-green-500 mt-5'>Learn More</button>
                   </div>
                 </div>
